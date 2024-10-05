8c6750b2705a5728363370b222ce5fac97d3fd38
Prath@7057 <iamprathpatil@gmail.com>
2024-10-06 00:22:39 +0530

    Commite By Prathamesh

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     @yield('style')
 </head>
+{{-- testing changes --}}
 <body>
     <div class="container">
         @yield('content')
